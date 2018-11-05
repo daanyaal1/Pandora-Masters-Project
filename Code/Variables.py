df769ff252469f6a8f9bb7ef7cf437e4d97c05ad
daanyaal1 <daanyaalchaudry70@Gmail.com>
2018-11-05 19:13:59 +0000

    Add files via upload

--- Code/Variables.py
+++ Code/Variables.py
@@ -6,11 +6,28 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
+def get_idList(df):
+    idList = []
+    for i in range(max(df.event)):
+        new_df= df.loc[df['event']==i+1]
+        x = np.array(new_df.x).reshape(-1,1)
+        y = np.array(new_df.z).reshape(-1,1)
+        if x.shape==(0,1) or y.shape==(0,1):
+            continue
+        idList.append(new_df['id'[0]])
+        return idList
+                            
+
 def get_nHits(df):
     "returns nHits for each event"
     nHits=[]
     for i in range(max(df.event)):
-        nHits.append(len(df.loc[df['event']==i+1]))
+        new_df= df.loc[df['event']==i+1]
+        x = np.array(new_df.x).reshape(-1,1)
+        y = np.array(new_df.z).reshape(-1,1)
+        if x.shape==(0,1) or y.shape==(0,1):
+            continue
+        nHits.append(len(new_df))
     return nHits
 
 def RSS(df):
@@ -20,6 +37,8 @@ def RSS(df):
         new_df = df.loc[df['event']==i+1]
         x = np.array(new_df.x).reshape(-1,1)
         y = np.array(new_df.z).reshape(-1,1)
+        if x.shape==(0,1) or y.shape==(0,1):
+            continue
         reg = LinearRegression().fit(x, y)
         y_pred = reg.predict(x)
         rssArray.append(mean_squared_error(y, y_pred))
@@ -29,8 +48,16 @@ def pc2VarExplained(df):
     '''2nd pc should explain more variance for showers then tracks'''
     pcaVar = []
     scaler = StandardScaler()
+    count = 1
+    #nHits = get_nHits(df)
     for i in range(max(df.event)):
+        print(count)
+        count= count+1
         new_df = df.loc[df['event']==i+1]
+        x = np.array(new_df.x).reshape(-1,1)
+        y = np.array(new_df.z).reshape(-1,1)
+        if x.shape==(0,1) or y.shape==(0,1):
+            continue
         scaler.fit(new_df[['x','z']])
         new_df[['x', 'z']] = scaler.transform(new_df[['x','z']])
         data = [[np.array(new_df.x)[i], np.array(new_df.z)[i]] for i in range(len(new_df.x))]
